877a63585784b41616a5804e9932ff5af39d52bf
Niminem <leon.l.lysak@gmail.com>
2024-07-10 21:28:27 -0400

    reduced scopes, added test funcs for run cmd

--- example/test.nim
+++ example/test.nim
@@ -1,3 +1,17 @@
+import std/[jsconsole, jsffi]
 
-proc returnTest: cstring {.exportc.} = # call this func with 'run' command
+template log(body) =
+    console.log(body)
+
+proc returnTest: cstring {.exportc.} = # call this func with nasp run --func:"returnTest"
     return cstring"Hello, World!"
+
+proc noReturnTest {.exportc.} = # call this w/ run cmd & then use nasp open --logs to see output
+    log "what up"
+
+proc singleParamTest(p: int): int {.exportc.} = # call w/ nasp run --func:"singleParamTest" --args:'[10]'
+    return p + p
+
+proc shouldBreak {.exportc.} = # call w/ nasp run --func:"shouldBreak" (obviously this will not throw err)
+    var nonExistantObj {.importc,nodecl.}: JsObject
+    log nonExistantObj
